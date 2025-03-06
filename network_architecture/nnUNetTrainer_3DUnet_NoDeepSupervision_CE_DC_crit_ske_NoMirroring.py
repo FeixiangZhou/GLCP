7cfcbb7c9870517b18a29e08b836f919d094da72
FeixiangZhou <72953315+FeixiangZhou@users.noreply.github.com>
2025-03-06 19:49:21 +0000

    Add files via upload

---
+++ network_architecture/nnUNetTrainer_3DUnet_NoDeepSupervision_CE_DC_crit_ske_NoMirroring.py
@@ -0,0 +1,16 @@
+
+
+from nnunetv2.training.nnUNetTrainer.variants.network_architecture.nnUNetTrainer_3DUnet_NoDeepSupervision_CE_DC import  nnUNetTrainer_3DUnet_NoDeepSupervision_CE_DC
+
+
+
+
+
+class nnUNetTrainer_3DUnet_NoDeepSupervision_CE_DC_crit_ske_NoMirroring(nnUNetTrainer_3DUnet_NoDeepSupervision_CE_DC):
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
+        
